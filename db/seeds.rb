bac2dd6914dc27c71906cb1ea9fbce8f0445bea9
sebastian medina <sebastianlopez_21@hotmail.com>
2023-03-13 11:44:27 -0400

    Fixes seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,57 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+
+Area.create([
+    {
+        area_name: "Information Technology"
+    },
+    {
+        area_name: "Human Resources"
+    },
+    {
+        area_name: "Marketing"
+    },
+    {
+        area_name: "Sales"
+    },
+    {
+        area_name: "Accounting"
+    }
+])
+
+
 Position.create([
     {
         position_name: "Software Developer",
-        salary: "112.000 p/year",
-        area: "Information Technology"
+        salary: 112.000,
+        area_name: "Information Technology",
+        area_id: 1
     },
     {
         position_name: "Human Resources Manager",
-        salary: "80.000 p/year",
-        area: "Human Resources"
+        salary: 80.000 ,
+        area_name: "Human Resources",
+        area_id: 2
     },
     {
         position_name: "Marketing Manager",
-        salary: "90.000 p/year",
-        area: "Marketing"
+        salary: 90.000,
+        area_name: "Marketing",
+        area_id: 3
     },
     {
         position_name: "Sales Representative",
-        salary: "87.000 p/year",
-        area: "Sales"
+        salary: 87.000,
+        area_name: "Sales",
+        area_id: 4
+
     },
     {
         position_name: "Accountant",
-        salary: "75.000 p/year",
-        area: "Accounting"
+        salary: 75.000,
+        area_name: "Accounting",
+        area_id: 5
     }
 
 ])
