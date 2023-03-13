2b974d200193f7759df23892c215c97f4bff6db1
sebastian medina <sebastianlopez_21@hotmail.com>
2023-03-13 10:56:49 -0400

    Adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,34 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+Position.create([
+    {
+        position_name: "Software Developer",
+        salary: "112.000 p/year",
+        area: "Information Technology"
+    },
+    {
+        position_name: "Human Resources Manager",
+        salary: "80.000 p/year",
+        area: "Human Resources"
+    },
+    {
+        position_name: "Marketing Manager",
+        salary: "90.000 p/year",
+        area: "Marketing"
+    },
+    {
+        position_name: "Sales Representative",
+        salary: "87.000 p/year",
+        area: "Sales"
+    },
+    {
+        position_name: "Accountant",
+        salary: "75.000 p/year",
+        area: "Accounting"
+    }
+
+])
+
 
 puts "✅ Done seeding!"
